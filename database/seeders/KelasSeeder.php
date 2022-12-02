d4fc9091363fa770d833adce191d86433e707041
unknown <rizkifahrezi990@gmail.com>
2022-12-02 11:00:39 -0800

    seeder kelas & pemilih

---
+++ database/seeders/KelasSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class KelasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+ 
+    	for($i = 1; $i <= 50; $i++){
+ 
+    	      // insert data ke table pegawai menggunakan Faker
+    		DB::table('pemilihs')->insert([
+    			'nama_kelas' => 'XII RPL' . $i,
+    		]);
+        }
+    }
+}
